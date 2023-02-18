cd4219aa3b4a0602b7a3e7cfb841cdb16ee612e2
Leandro de Azevedo <leousanet@hotmail.com>
2023-02-18 20:08:08 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp_ProcuraCaractere/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp_ProcuraCaractere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Leandro de Azevedo";
+
+            int count = 0;
+            foreach (var item in str)
+            {
+                if (item == 'a' || item == 'A')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(" Foram encontrados " + count +  " caracteres ");
+
+            Console.ReadKey();
+        }
+    }
+}
